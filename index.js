7fb69b2e05588dc9260a956693599a98f895108d
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-26 22:50:02 +0300

    Completed assignemt

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(word) { 
+
+  word = word.replace(/ /g,"").toLowerCase();
+  let compareword = word.split("").reverse().join("");
+
+  if (compareword === word) {
+    return true;
+  } 
+  else {
+    return false;
+  } 
+
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
